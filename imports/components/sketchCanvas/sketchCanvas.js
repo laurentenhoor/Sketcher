d2e1e5b9adad67ae998ed9605b664fec6a91809e
Lauren ten Hoor <laurentenhoor@gmail.com>
2017-10-21 22:02:53 +0200

    Initial stable sketch canvas

---
+++ imports/components/sketchCanvas/sketchCanvas.js
@@ -0,0 +1,29 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+import 'literallycanvas';
+
+import template from './sketchCanvas.html';
+
+
+class SketchCanvasController {
+	
+	constructor() {
+		
+		LC.init(document.getElementsByClassName('my-drawing')[0],
+				{imageURLPrefix: '/literallycanvas_img'}
+		);
+		
+	}
+	
+	
+	
+}
+
+export default angular.module('sketcher.sketchCanvas', [
+  angularMeteor,
+])
+.component('sketchCanvas', {
+	templateUrl : template,
+	controller: [SketchCanvasController],
+});
