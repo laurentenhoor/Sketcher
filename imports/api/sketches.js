0d81ce15dc1b02583129f1a1588f051ccd64c1b7
Lauren ten Hoor <laurentenhoor@gmail.com>
2017-10-22 23:52:39 +0200

    Publish database

--- imports/api/sketches.js
+++ imports/api/sketches.js
@@ -1,3 +1,9 @@
 import { Mongo } from 'meteor/mongo';
  
 export const Sketches = new Mongo.Collection('sketches');
+
+if(Meteor.isServer) {
+    Meteor.publish('sketches', function(){
+        return Posts.find({});
+    });
+}
