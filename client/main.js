ea458c6ecdb08758d95bdb49e6c385c15b7e29ea
Lauren ten Hoor <laurentenhoor@gmail.com>
2017-10-23 17:47:56 +0200

    Working session management in main.js

--- client/main.js
+++ client/main.js
@@ -2,49 +2,14 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import angularRoute from 'angular-route';
 
+import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random'
+import { Cookies } from 'meteor/ostrio:cookies';
 
 import sketchCanvas from '../imports/components/sketchCanvas/sketchCanvas';
 import sketchMenu from '../imports/components/sketchMenu/sketchMenu';
 
 import { Sketches } from '../imports/api/sketches.js';
-
-
-class MainController {
-
-	constructor($rootScope, $scope, $routeParams, $location)  {
-
-		$scope.$on('$routeChangeSuccess', function() {
-
-			console.log($routeParams.canvasId);
-			console.log(localStorage.getItem('canvasId'));
-			
-			var canvasId = $routeParams.canvasId;
-			if (!canvasId) {
-				canvasId = localStorage.getItem('canvasId');
-				$location.url('/'+canvasId);
-			}			
-			console.log('found id from url or localStorage: '+canvasId)
-			
-			Meteor.call('canvasExists', canvasId, function(err, canvasExists) {
-			
-				if (canvasExists) {
-					console.log('we found an existing canvas in the backend')	
-				} else {
-					canvasId = Random.id();
-					console.log('canvasId created: '+ canvasId);
-					
-				}	
-
-				localStorage.setItem('canvasId', canvasId);
-				$rootScope.canvasId = canvasId
-				
-			}) 
-
-		});
-
-	}
-}
 
 
 angular.module('sketcher', [
@@ -57,7 +22,59 @@ angular.module('sketcher', [
 
 	])
 
-	.config(function($routeProvider, $locationProvider) {
+	.controller('MainController', ['$rootScope', '$scope', '$routeParams', '$location',
+		function($rootScope, $scope, $routeParams, $location) {
+		
+		const cookies = new Cookies();
+		
+		$scope.$on('$routeChangeSuccess', function() {
+
+			console.log('from url: '+$routeParams.canvasId);
+			console.log('from localStorage: '+localStorage.getItem('canvasId'));
+			console.log('from cookies: '+cookies.get('canvasId'));
+			
+			var canvasId = $routeParams.canvasId || localStorage.getItem('canvasId') || cookies.get('canvasId');
+			if (!$routeParams.canvasId) {
+				$location.url('/'+canvasId);
+				return;
+			}
+			
+			console.log('found id from url, localStorage or cookie: ' + canvasId)
+			
+			
+			Meteor.call('canvasExists', canvasId, function(err, canvasExists) {
+			
+				if (canvasExists) {
+					
+					console.log('we found an existing canvas in the backend')
+					
+				} else {
+					
+					console.log('canvasId does not exist in database')
+					
+					canvasId = localStorage.getItem('canvasId') || cookies.get('canvasId') || Random.id();
+					Sketches.insert({canvasId: canvasId});
+					
+					console.log('canvasId created in database: '+ canvasId);
+					
+					$location.url('/'+canvasId);
+					return;
+					
+				}
+				
+				localStorage.setItem('canvasId', canvasId);
+				cookies.set('canvasId', canvasId);
+				
+				$rootScope.canvasId = canvasId;
+				console.log('active canvas: '+canvasId);
+				
+			});
+
+		});
+		
+	}])
+	
+	.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
 		$locationProvider.html5Mode(true);
 		$locationProvider.hashPrefix('!');
@@ -70,7 +87,4 @@ angular.module('sketcher', [
 			controller : 'MainController'
 		});	
 
-	})
-
-	.controller('MainController', ['$rootScope', '$scope','$routeParams', '$location', MainController]);
-
+	}]);
