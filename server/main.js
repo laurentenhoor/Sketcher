3fb8dcae3fae3510336abc652a64b157944f5f7b
Lauren ten Hoor <laurentenhoor@gmail.com>
2017-10-28 17:11:34 +0200

    Fixed issue for undefined canvas loading

--- server/main.js
+++ server/main.js
@@ -5,8 +5,5 @@ import { Sketches } from '../imports/api/sketches.js';
 Meteor.startup(() => {
   // code to run on server at startup
 	
-	Sketches.remove({canvasId: 'undefined'});
-	Sketches.remove({canvasId: ''});
-	Sketches.remove({canvasId: null});
 
 });
